57b593dd5fed9874c067c3b2ede12428783180b8
우동진 <wjinh93@gmail.com>
2021-11-12 22:19:03 +0900

    removed secret key

--- LCM/settings.py
+++ LCM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kyqcl+7c*+hxip)hf8@y03ms$5m3^32#2p#k$m(#n0ltdmp%+y'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
